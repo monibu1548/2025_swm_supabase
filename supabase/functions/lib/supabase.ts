8a0e424f9c82cc4c824ddf948900021f2db42dff
정진규 <monibu1548@gmail.com>
2025-05-05 18:43:17 +0900

    insert 예제

--- supabase/functions/lib/supabase.ts
+++ supabase/functions/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import { supabaseProjectURL, supabaseServiceRoleKey } from "../environments";
-import { Database } from "../types/supabase";
+import { supabaseProjectURL, supabaseServiceRoleKey } from "../environments.ts";
+import { Database } from "../types/supabase.ts";
 
 export const supabase = createClient<Database>(
   supabaseProjectURL,
